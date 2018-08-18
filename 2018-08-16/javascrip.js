e10256cc39c34e90762e99e08c409c84ced12633
unknown <datnguyen306>
2018-08-18 15:21:45 +0700

    baitapjavamoi

---
+++ 2018-08-16/javascrip.js
@@ -0,0 +1,11 @@
+var img =document.getElementById('bongden');
+  function battat()
+  {
+      if (img.src ="pic_buldoff.gif") {
+        img.src='pic_bulboff.gif';
+      }
+      else {
+        img.src ='pic_buldon.gif';
+      }
+  }
+  
