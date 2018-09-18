b72f64aa5a0603580f84e201ddebae642173ea80
unknown <datnguyen306>
2018-09-18 18:59:47 +0700

    baitap

--- php/2018-09-15/form dang nhap/form.php
+++ php/2018-09-15/form dang nhap/form.php
@@ -10,45 +10,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
   </head>
   <body>
-    <form action="" method="post">
+    <form action="wellcom.php" method="POST">
     Name: <input type="text" name="name"><br>
     E-mail: <input type="text" name="email"><br>
     <input type="submit">
     </form>
-  <?php
-// $server_name="192.168.60.141";
-// $user_name="nam";
-// $password="123456";
-// $dbname="aptech_php_14";
-$server_name="localhost";
-$user_name="root";
-$password="";
-$dbname="aptech_php_14_hocketnoi";
-$test_connect = mysqli_connect($server_name,$user_name,$password,$dbname);
-// if($test_connect){
-//     echo"ket noi thanh cong";
-//     echo "<br>";
-// }
-// else{
-//     echo"kết nối không thành công";
-//     echo"<br>";
-// }
-if(isset($_POST['submit'])){
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $sql="select * from users where name=$name and email=$email";
-  if(mysqli_query($test_connect,$sql)!=0){
-    echo"dang nhap thanh cong";
-}
-else{
-  echo"đăng nhap khong thanh cong";
-}
-}
-mysqli_close($test_connect);
   
-  ?>
-  </tbody>
-</table> 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- php/2018-09-15/form dang nhap/wellcom.php
+++ php/2018-09-15/form dang nhap/wellcom.php
@@ -1,4 +1,34 @@
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-<?php $name=mysqli_real_escape_string($test_connect,$_POST['name']);?>
-<?php $email=mysqli_real_escape_string($test_connect,$_POST['email']);?>
+<?php
+// $server_name="192.168.60.141";
+// $user_name="nam";
+// $password="123456";
+// $dbname="aptech_php_14";
+$server_name="localhost";
+$user_name="root";
+$password="";
+$dbname="aptech_php_14_hocketnoi";
+$test_connect = mysqli_connect($server_name,$user_name,$password,$dbname);
+if($test_connect){
+    echo"ket noi thanh cong";
+    echo "<br>";
+}
+else{
+    echo"kết nối không thành công";
+    echo"<br>";
+}
+//   echo $_POST['name'];
+//   echo $_POST['email'];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $sql="SELECT * FROM users WHERE name='".$name."' and email='".$email."'";
+  $result=mysqli_query($test_connect,$sql);
+  if(mysqli_num_rows($result)>0){
+    echo"dang nhap thanh cong";
+  }
+  else{
+    echo"đăng nhap khong thanh cong";
+    }
+// }
+mysqli_close($test_connect);
+  
+  ?>
